f96ff39d33cc2bf7f7f2d82fa513aa648482bffe
rubenromeomartinez <ruben.romeo.martinez@gmail.com>
2026-03-03 17:58:37 -0600

    Ajustes a front

--- client/server.js
+++ client/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 
-const PORT = process.env.PORT || 3000; // 8080 para el cliente
+const PORT = process.env.PORT || 8080; // 8080 para el cliente
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Cliente web  corriendo en el puerto ${PORT}`);
